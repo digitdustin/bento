ddac9d297eecffa21d3f2f469f563c6b87d09851
Justin Schwartz <schw.justin@gmail.com>
2022-11-22 17:57:36 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				sentiment: {
-					red: "#E54545",
-					yellow: "#F2C230",
-					green: "#44CC29",
-				},
-				gray: {
-					10: "#18181A",
-					30: "#494A4D",
-					40: "#616266",
-					50: "#797A80",
-					60: "#919399",
-					70: "#AAABB3",
-					98: "#F5F6FA",
-				},
+				
 			},
 		},
 	},
